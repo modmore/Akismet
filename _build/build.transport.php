5a6edfe346fead0db4ac3d09ac153ab77433524c
digitalpenguin <murray@digitalpenguin.hk>
2021-11-12 11:07:55 +0800

    Akismet 1.2.1-pl release

--- _build/build.transport.php
+++ _build/build.transport.php
@@ -23,7 +23,7 @@ if (!defined('MOREPROVIDER_BUILD')) {
     /* define version */
     define('PKG_NAME', 'Akismet');
     define('PKG_NAMESPACE', 'akismet');
-    define('PKG_VERSION', '1.2.0');
+    define('PKG_VERSION', '1.2.1');
     define('PKG_RELEASE', 'pl');
 
     /* load modx */
